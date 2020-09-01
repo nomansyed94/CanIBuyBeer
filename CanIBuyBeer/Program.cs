e9dd58b2b54878187e95fb7fb25fe2a2b7f03823
noman syed <noman-syed@hotmail.com>
2020-09-01 10:31:03 +0200

    Add project files.

---
+++ CanIBuyBeer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CanIBuyBeer
+{
+    class Program
+    {
+        static int SafeNumberInput()
+        {
+            int number;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out number) == true)
+                    break;
+                Console.WriteLine("Var vänlig bara siffor tack!");
+            }
+
+            return number;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            var age = SafeNumberInput();
+            Console.WriteLine("Vilken location är du på? (systemet/krogen)");
+            var location = Console.ReadLine();
+            location = location.ToLower();
+            Console.WriteLine("Har du leg? (J/N)");
+            var leg = Console.ReadLine();
+            Console.WriteLine("Vilken promillehalt har du?");
+            decimal promille = Convert.ToDecimal(Console.ReadLine());
+
+            //if (age > 17 && locationA == "krogen" && leg == "J" && promille < 1.0m ||
+            //    age > 19 && locationB == "systemet" && leg == "J" && promille < 1.0m)
+            //{
+            //    Console.WriteLine("Du får köpa Sharaab.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Nej du får inte köpa Sharaab.");
+            //}
+
+            bool canIBuyBeer = age > 17 && location == "krogen" && leg == "J" && promille < 1.0m ||
+                age > 19 && location == "systemet" && leg == "J" && promille < 1.0m;
+            if (canIBuyBeer == true)
+            {
+                Console.WriteLine("Du kan köpa Öl.");
+            }
+            else
+            {
+                Console.WriteLine("Du kan inte köpa Öl.");
+            }
+        }
+    }
+}
